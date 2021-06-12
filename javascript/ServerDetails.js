663cac7c13afc6928a328581b6f288aeb655885a
Belugula Shushanth <sushanth179@gmail.com>
2021-06-12 14:05:55 +0530

    last change

--- javascript/ServerDetails.js
+++ javascript/ServerDetails.js
@@ -1,9 +1,5 @@
-serverDetails = {
-<<<<<<< HEAD
+serverDetails ={
     frontEndUrl : 'https://belugulashushanth.github.io/EmployeeManagementSystem', 
-=======
-    frontEndUrl : 'https://belugulashushanth.github.io/EmployeeManagementSystem',
->>>>>>> 2228e9062afe28368a383dfae7b9b396ac31442d
     backEndUrl : 'http://localhost:8080',
     enabled : true
 }
