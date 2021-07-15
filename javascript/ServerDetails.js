2ff682554921d8a79902bab1caaeb2d0f99013ca
Belugula Shushanth <66299354+BelugulaShushanth@users.noreply.github.com>
2021-07-15 11:54:39 +0530

    Update ServerDetails.js

--- javascript/ServerDetails.js
+++ javascript/ServerDetails.js
@@ -1,5 +1,5 @@
 serverDetails ={
     frontEndUrl : 'https://belugulashushanth.github.io/EmployeeManagementSystem', 
-    backEndUrl : 'http://localhost:8080',
+    backEndUrl : 'http://localhost:8080' ,
     enabled : true
 }
