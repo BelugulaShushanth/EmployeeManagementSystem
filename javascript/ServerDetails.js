1c4659e975550343f02881cc797546cf728b8c82
Belugula Shushanth <66299354+BelugulaShushanth@users.noreply.github.com>
2022-12-11 15:35:31 +0530

    Update ServerDetails.js

--- javascript/ServerDetails.js
+++ javascript/ServerDetails.js
@@ -1,5 +1,5 @@
 serverDetails ={
-    frontEndUrl : 'https://belugulashushanth.github.io/EmployeeManagementSystem', 
+    frontEndUrl : 'https://belugulashushanth.netlify.app', 
     backEndUrl : 'https://ab68-2405-201-c017-ba3a-10d-de61-4e55-5138.in.ngrok.io' ,
     enabled : true
 }
